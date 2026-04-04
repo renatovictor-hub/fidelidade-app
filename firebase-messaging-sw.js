252dce53a7e05a79ea353558ae51e226d22f9406
renatovictor-hub <renato.victor@gmail.com>
2026-04-04 16:23:00 -0500

    Upgrade Firebase SDK to version 9.22.0

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,14 +1,12 @@
-importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
- messagingSenderId: "373498571854"
+  apiKey: "AIzaSyBIsxfTBBWpO8J5UvcWbxIXtL68SDKfGjQ",
+  authDomain: "fidelidade-app-9671c.firebaseapp.com",
+  projectId: "fidelidade-app-9671c",
+  messagingSenderId: "373498571854",
+  appId: "1:373498571854:web:640b54cdae0cbd9790271f"
 });
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(function(payload) {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-  });
-});
