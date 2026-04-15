2448c121fd0b458f932b1155588c19ce401ab5f8
renatovictor-hub <renato.victor@gmail.com>
2026-04-15 01:55:44 -0500

    Update authorization token in sendPush.js

--- api/sendPush.js
+++ api/sendPush.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": "Bearer os_v2_app_cd6qqerxb5aivhvfzozut5owgwukxlod3ctuw5v2cep4jyxn46xk6urqt76rnkbwlzgm3ebgvytorglcc4wzkylfucyfldveye2omxq"
+        "Authorization": "Bearer os_v2_app_cd6qqerxb5aivhvfzozut5owgvnremvccsduz3nf2fe2xxyxyxpoeh4p62regkczzri2vmdrydgqujcjwafcmggvgxr5fj6n4shvk6a"
       },
       body: JSON.stringify({
         app_id: "10fd0812-370f-408a-9ea5-cbb349f5d635",
