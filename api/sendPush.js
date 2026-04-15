dec63db7ac0940a6e0918079bbcb0f355452f882
renatovictor-hub <renato.victor@gmail.com>
2026-04-15 15:17:20 -0500

    Refactor push notification to include user IDs
    Fetch user IDs from Firebase and update push notification logic.

--- api/sendPush.js
+++ api/sendPush.js
@@ -4,21 +4,33 @@ export default async function handler(req, res) {
 
     const { titulo, desc, link } = req.body;
 
+    // 🔥 BUSCAR USUÁRIOS NO FIREBASE
+    const usersRes = await fetch("https://fidelidade-app-9671c-default-rtdb.firebaseio.com/users.json");
+    const usersData = await usersRes.json();
+
+    if (!usersData) {
+      return res.status(400).json({ error: "Sem usuários" });
+    }
+
+    // 🔥 PEGAR TODOS OS IDS
+    const userIds = Object.values(usersData).map(u => u.user_id);
+
+    console.log("Usuarios:", userIds);
+
+    // 🔥 ENVIAR PUSH
     const response = await fetch("https://api.onesignal.com/notifications", {
       method: "POST",
       headers: {
-        "Authorization": "Basic os_v2_app_cd6qqerxb5aivhvfzozut5owgwsw3t3hf33eq25xyhvnber2szipgvyktxaybkklriiuzrdmvvymj7mwhktbqze4w32erd2odxtlmba",
+        "Authorization": "Bearer os_v2_app_cd6qqerxb5aivhvfzozut5owgw2lnapwbmreg5uz7z76ur5il6vwde4sxehko5vrupfgj4zwvqy4najznu5a3bnljpyxsos7nieawlq",
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-
         app_id: "10fd0812-370f-408a-9ea5-cbb349f5d635",
 
         target_channel: "push",
 
-        // 🔥 ESSA LINHA É O SEGREDO
         include_aliases: {
-          external_id: ["all"]
+          external_id: userIds
         },
 
         headings: {
@@ -36,7 +48,7 @@ export default async function handler(req, res) {
 
     const data = await response.json();
 
-    console.log("OneSignal:", data);
+    console.log("Resposta OneSignal:", data);
 
     if (!response.ok) {
       return res.status(400).json(data);
@@ -48,4 +60,5 @@ export default async function handler(req, res) {
     console.error(err);
     return res.status(500).json({ error: "Erro interno" });
   }
+
 }
