216c946eebe0cdacbf9675d9d4927284f26d89a9
renatovictor-hub <renato.victor@gmail.com>
2026-04-15 02:21:12 -0500

    Update sendPush.js

--- api/sendPush.js
+++ api/sendPush.js
@@ -1,35 +1,37 @@
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
-  const { titulo, desc, link } = req.body;
-
   try {
+
+    if (req.method !== "POST") {
+      return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    const { titulo, desc, link } = req.body || {};
+
+    if (!titulo || !desc) {
+      return res.status(400).json({ error: "Faltando dados" });
+    }
 
     const response = await fetch("https://api.onesignal.com/notifications", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer os_v2_app_cd6qqerxb5aivhvfzozut5owgw7eg6jube3up3nmw45ydck3i4cdr2ij2cwvuocasf3mlcyiz4k7v7xh4tc55tiet4vkjxhdt7ml3sy"
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": "Bearer os_v2_app_cd6qqerxb5aivhvfzozut5owgwukxlod3ctuw5v2cep4jyxn46xk6urqt76rnkbwlzgm3ebgvytorglcc4wzkylfucyfldveye2omxq"
       },
       body: JSON.stringify({
         app_id: "10fd0812-370f-408a-9ea5-cbb349f5d635",
 
         target_channel: "push",
 
-        // 🔥 CORRETO
+        // 🔥 esse funciona na prática
         included_segments: ["All"],
 
         headings: {
-          en: titulo,
-          es: titulo
+          en: titulo
         },
 
         contents: {
-          en: desc,
-          es: desc
+          en: desc
         },
 
         web_url: link
@@ -41,21 +43,20 @@ export default async function handler(req, res) {
     console.log("OneSignal response:", data);
 
     if (!response.ok) {
-  console.error("ERRO ONESIGNAL:", data);
-  return res.status(400).json({
-    erro: data
-  });
-}
+      return res.status(400).json({
+        erro: data
+      });
+    }
 
     return res.status(200).json({
       success: true,
-      data: data
+      data
     });
 
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error("ERRO SERVIDOR:", err);
     return res.status(500).json({
-      error: "Erro ao enviar push"
+      error: "Erro interno"
     });
   }
 }
